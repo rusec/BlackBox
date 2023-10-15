3f05f89d941b5126b305055065b6a81d1449e385
GlaceDa <143040684+GlaceDa@users.noreply.github.com>
2023-10-15 15:57:31 -0400

    Update NTP-PS.ps1

--- Noodle/NTP-PS.ps1
+++ Noodle/NTP-PS.ps1
@@ -18,3 +18,27 @@ $NTPServers = "time.google.com,0x1"
 w32tm /config /manualpeerlist:$NTPServers /syncfromflags:manual /reliable:YES /update
 Restart-Service w32time
 w32tm /resync
+
+###BRAIN
+
+Get-Service w32time | Set-Service -StartupType Automatic
+
+# Start the Windows Time service if not already running
+if ((Get-Service w32time).Status -ne 'Running') {
+    Start-Service w32time
+}
+
+ #(default Microsoft servers used here, you can replace this)
+$NTPServers = "time.windows.com,0x1"
+
+
+w32tm /config /manualpeerlist:$NTPServers /syncfromflags:manual /reliable:YES /update
+
+
+Restart-Service w32time
+
+w32tm /resync /rediscover
+
+w32tm /query /status
+
+Write-Output "NTP configuration updated and time resynchronized."
