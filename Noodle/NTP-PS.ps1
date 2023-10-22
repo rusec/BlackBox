a4e8df38b45ed3b03f95990ad29ff6ba22296ea4
GlaceDa <143040684+GlaceDa@users.noreply.github.com>
2023-10-22 11:57:54 -0400

    Update NTP-PS.ps1

--- Noodle/NTP-PS.ps1
+++ Noodle/NTP-PS.ps1
@@ -1,4 +1,6 @@
-##VERSION 1
+#VERSION 2 WORKS FOR WINDOWS! BALLER! currently can use 4 ntp servers subject have more or less added
+
+Get-Service w32time | Set-Service -StartupType Automatic
 
 Get-Service w32time | Set-Service -StartupType Automatic
 
@@ -7,10 +9,33 @@ if ((Get-Service w32time).Status -ne 'Running') {
     Start-Service w32time
 }
 
- $NTPServers = "time.windows.com,0x1"
+
+$DefaultNTPServers = "time.windows.com,0x1"
+
+$UserInput = Read-Host "Please enter an NTP to connect to: NIST-EST, NIST-MST, ProxMox, Cloudflare"
+
+$UserDefinedNTPServers 
+
+switch ($UserInput) {
+    "NIST-EST" {
+        $UserDefinedNTPServers = "time-a-g.nist.gov"   #NIST NTP MARYLAND (UTC-5)
+    }
+    "NIST-MST" {
+        $UserDefinedNTPServers = "time-a-wwv.nist.gov"   #NIST NTP COLORADO (UTC-7)
+    }
+    "ProxMox" {
+        $UserDefinedNTPServers = "128.6.1.1"  #Proxmox NTP sevrer 
+    }
+    "Cloudflare"{
+        $UserDefinedNTPServers = "time.cloudflare.com"  #Cloudfare
+    }
+    default {
+        $UserDefinedNTPServers = $DefaultNTPServers #Default NTP to be always set back to windows 
+    }
+}
 
 
-w32tm /config /manualpeerlist:$NTPServers /syncfromflags:manual /reliable:YES /update
+w32tm /config /manualpeerlist:$UserDefinedNTPServers /syncfromflags:manual /reliable:YES /update
 
 
 Restart-Service w32time
@@ -20,3 +45,9 @@ w32tm /resync /rediscover
 w32tm /query /status
 
 Write-Output "NTP configuration updated and time resynchronized."
+
+
+##CHECK YOU'RE ON THE SELECETED NTP SEVER 
+$ntpServers = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" -Name NtpServer | Select-Object -ExpandProperty NtpServer
+
+Write-Output "Current NTP Server(s): $ntpServers"
