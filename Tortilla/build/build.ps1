bb3ea45b49c62bd1900dc9ec59e7d9c7aad8836c
dominusmars <khawammohamad99@gmail.com>
2024-01-18 16:35:02 -0500

    Updating Binaries

--- Tortilla/build/build.ps1
+++ Tortilla/build/build.ps1
@@ -9,9 +9,9 @@ catch {
 }
 tsc  
 try {
-    pkg --out-path releases/latest/x64 -t 'node16-linux-x64,node16-win-x64,node16-macos-x64' ./dist/src/Tortilla.js
+    pkg --out-path releases/latest/x64 -t 'node16-linux-x64,node16-win-x64,node16-macos-x64' -c ./package.json ./dist/src/Tortilla.js
     Write-Output 'Finished x64'
-    pkg --out-path releases/latest/latest -t 'node16-linux,node16-win,node16-macos' ./dist/src/Tortilla.js
+    pkg --out-path releases/latest/latest -t 'node16-linux,node16-win,node16-macos' -c ./package.json ./dist/src/Tortilla.js
     Write-Output 'Finished latest'
     # pkg --out-path releases/latest/arm -t 'node16-linux-arm64,node16-macos-arm64' ./dist/src/Tortilla.js
     Write-Output 'UNABLE TO CREATE ARM Binaries PLEASE RUN pkg --out-path releases/latest/arm -t "node16-linux-arm64,node16-macos-arm64" ./dist/src/Tortilla.js'
