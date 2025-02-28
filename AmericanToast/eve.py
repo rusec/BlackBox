954ce41aa15416cc6d2607d09a8fabe4493d4f22
dominusmars <khawammohamad99@gmail.com>
2025-02-27 22:50:41 -0500

    added suricata dir

--- AmericanToast/eve.py
+++ AmericanToast/eve.py
@@ -9,9 +9,20 @@ from subprocess import check_output
 
 hostname = argv[1]
 port = int(argv[2])
+suricata_dir = (argv[3]) if len(argv) > 3 else '/var/log/suricata/eve.json'
 start = 1
 
+if len(argv) != 3:
+    print("Usage: eve.py <hostname> <port> <suricata_dir>")
+    exit(1)
+
+
+
 if not exists('/traffic'): mkdir('/traffic')
+
+if not suricata_dir:
+    print(f"Suricata directory {suricata_dir} does not exist.")
+    exit(1)
 
 if exists('/traffic/eve.json'):
     check_output(['sed', '$d', '/traffic/eve.json'])
@@ -19,4 +30,4 @@ if exists('/traffic/eve.json'):
 
 conn = Connection(hostname, port=port)
 with open('/traffic/eve.json', 'a') as f:
-    res = conn.run(f'tail -f -n +{start} /var/log/suricata/eve.json', out_stream=f)
+    res = conn.run(f'tail -f -n +{start} {suricata_dir}/eve.json', out_stream=f)
